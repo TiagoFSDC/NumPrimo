ec92b2e78632353ad0a19baf7d1aac50f627fdb5
TiagoFSDC <tiagofsdc@gmail.com>
2023-03-05 19:33:27 -0300

    Add project files.

---
+++ NumPrimo/Program.cs
@@ -0,0 +1,26 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double n, count = 0;
+
+        Console.WriteLine("Digite um numero positivo qualquer:");
+        n = double.Parse(Console.ReadLine());
+
+        for (int i = 1; i <= n; i ++)
+        {
+            if (n % i == 0)
+            {
+                count += 1;
+            }
+        }
+        if(count == 2)
+        {
+            Console.WriteLine("O numero é primo");
+        }
+        else
+        {
+            Console.WriteLine("O numero não é primo");
+        }
+    }
+}
